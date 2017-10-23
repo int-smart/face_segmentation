702036a9b9a8c22eabed05d851e97da1fd9543fd
Abhishek Thakur <abhi.dtu11@gmail.com>
2017-10-22 21:05:52 -0400

    Initial Testing done

--- change_labels.py
+++ change_labels.py
@@ -9,42 +9,58 @@ from matplotlib import pyplot as plt
 
 test_label_dir = '/media/abhishek/B67A61587A611701/Users/Second/Desktop/Research/FCN/parts_lfw_funneled_gt_images/'
 test_image_dir = '/media/abhishek/B67A61587A611701/Users/Second/Desktop/Research/FCN/lfw_funneled/'
+cPickle_file_dir = '/media/abhishek/New Volume/'
 image_type = '.jpg'
-cPickle_file = 'saved_test'
+cPickle_file = 'saved_test_00'
 
-def get_test_data(test_label_dir,test_image_dir):
+def get_test_data(test_label_dir, test_image_dir):
+    i=0
     images_labels = []
     for file in os.listdir(test_label_dir):
-        path_to_label = test_label_dir+file
+        i=i+1
+        path_to_label = test_label_dir + file
         if os.path.isfile(path_to_label):
             if '.ppm' in file:
-                file = file.replace('.ppm','')
+                file = file.replace('.ppm', '')
                 file = file.split('_')
                 file_number = file[-1]
                 file_name = file[:-1]
                 file_name = '_'.join(file_name)
                 dir_name = file_name
-                file_name = file_name+'_'+str(file_number)+image_type
-            label = misc.imread(path_to_label)
-            label = label[:,:,1]
+                file_name = file_name + '_' + str(file_number) + image_type
+            label = Image.open(path_to_label)
+            label = label.resize((500, 500))
+            label = np.array(label, dtype=np.int32)
+            label = label[:, :, 1]
 
-            path_to_image = test_image_dir+dir_name+'/'+file_name
-            image = misc.imread(path_to_image)
-            #image_scipy = misc.imread('/media/abhishek/B67A61587A611701/Users/Second/Desktop/Research/FCN/parts_lfw_funneled_gt_images/Aaron_Peirsol_0001.ppm')
-            #print(image_scipy.shape)
+            path_to_image = test_image_dir + dir_name + '/' + file_name
+            image = Image.open(path_to_image)
+            image = image.resize((500, 500))
+            image = np.array(image, dtype=np.float32)
+            # image_scipy = misc.imread('/media/abhishek/B67A61587A611701/Users/Second/Desktop/Research/FCN/parts_lfw_funneled_gt_images/Aaron_Peirsol_0001.ppm')
+            # print(image_scipy.shape)
             # plt.imshow(image_scipy[:,:,1])
             # plt.show()
-            images_labels.append([image,label])
+            images_labels.append([image, label])
+        if i%50==0:
+            print("Done processing 1000 images and labels")
+            save(cPickle_file_dir, cPickle_file+"_0{}".format(i%2), images_labels)
+            # images_labels = []
+            break
+        print("I am done with file {}, {}".format(file,i))
+    print(len(images_labels))
     return images_labels
 
-def save(test_image_dir, cPickle_file, images_labels):
-    cPickle_file = test_image_dir+cPickle_file
+def save(cPickle_file_dir, cPickle_file, images_labels):
+    print("Saving file......")
+    cPickle_file = cPickle_file_dir+cPickle_file
     stream = gzip.open(cPickle_file, 'wb')
     cPickle.dump(images_labels, stream)
     stream.close()
 
 def load(cPickle_file):
-    stream = gzip.open(cPickle_file, "rb")
+    print("Loading the saved file.....")
+    stream = gzip.open(cPickle_file_dir+cPickle_file, "rb")
     model = cPickle.load(stream)
     stream.close()
     return model
@@ -57,29 +73,30 @@ def intersection_over_union(ground_truth, prediction):
 
 
 if __name__=='__main__':
-    if cPickle_file not in os.listdir(test_image_dir):
+    if cPickle_file not in os.listdir(cPickle_file_dir):
         data = get_test_data(test_label_dir,test_image_dir)
-        save(test_image_dir, cPickle_file, data)
         print("I am saving the file")
     else:
         data = load(cPickle_file)
         print("I am loading the file")
+    #print(data)
     image = data[0][0]
     label = data[0][1]
-    label = tf.convert_to_tensor(label)
-    iou = tf.metrics.mean_iou(label,label,2)
+    #label = tf.convert_to_tensor(label)
+    iou = intersection_over_union(label,label)
+
     print(iou)
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-        sess.run(tf.local_variables_initializer())
-        iou = sess.run([iou])
+    # init = tf.global_variables_initializer()
+    # with tf.Session() as sess:
+    #     sess.run(init)
+    #     sess.run(tf.local_variables_initializer())
+    #     iou = sess.run([iou])
     print("The mean iou is ",iou)
     #print("The length of the list is ",len(iou[0]))
 
     #print("The elements are ", iou[0][1])
-    #plt.figure(1)
-    #plt.imshow(image[:,:,1])
-    #plt.figure(2)
-    #plt.imshow(label)
-    #plt.show()
+    # plt.figure(1)
+    # plt.imshow(image[:,:,1])
+    # plt.figure(2)
+    # plt.imshow(label)
+    # plt.show()
