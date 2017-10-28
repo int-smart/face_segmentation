cef6a74f11f1f62d5d790aad4d74c79813888008
Akash Rastogi <akashr050@gmail.com>
2017-10-28 01:49:09 -0400

    Adding xml parsing r code and output

---
+++ xml_parsing.R
@@ -0,0 +1,40 @@
+library(XML)
+library(dplyr)
+setwd('C:/Users/Akash Rastogi/Desktop/eecs_542/ANNOTATION')
+
+get_temp <- function(file_path){
+  doc <- xmlParse(file_path)
+  temp_list <- xmlToList(doc)
+  temp_names <- names(temp_list)[!(names(temp_list) %in% "object")]
+  data <- data.frame(temp_list[[temp_names[1]]])
+  names(data) <- temp_names[1]
+  for (j in c(2:length(temp_names))){
+    i = temp_names[j]
+    temp_data <- data.frame(temp_list[[i]])
+    names(temp_data) <- i
+    if (length(temp_list[[i]]) > 1){
+      temp_data <- as.data.frame(temp_list[[i]])
+      names(temp_data) <- names(temp_list[[i]])
+    }
+    data <- bind_cols(data, temp_data)
+  }
+  temp_obj <- temp_list$object[!names(temp_list$object) %in% c("part", "bndbox")]
+  temp_object <- as.data.frame(temp_obj)
+  temp_fnl <- merge(data, temp_object)
+}
+
+# list of files
+list_files <- list.files()
+fnl_parsed_data <- get_temp(list_files[1])
+for (i in c(2:length(list_files))){
+  temp_data <- get_temp(list_files[i])
+  fnl_parsed_data <- bind_rows(fnl_parsed_data, temp_data)
+}
+fnl_parsed_data$occluded[is.na(fnl_parsed_data$occluded)] <- 0
+fnl_parsed_data$width <- as.integer(fnl_parsed_data$width)
+fnl_parsed_data$height <- as.integer(fnl_parsed_data$height)
+fnl_parsed_data$depth <- as.integer(fnl_parsed_data$depth)
+fnl_parsed_data$segmented <- as.integer(as.character(fnl_parsed_data$segmented))
+fnl_parsed_data$truncated <- as.integer(fnl_parsed_data$truncated)
+fnl_parsed_data$difficult <- as.integer(fnl_parsed_data$difficult)
+fnl_parsed_data$occluded <- as.integer(fnl_parsed_data$occluded)
